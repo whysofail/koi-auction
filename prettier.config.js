9faf8431e4238987180230d858e504fb4b101d12
Alieza Nuriman <115769623+Aliezan@users.noreply.github.com>
2024-11-14 02:50:32 +0700

    feat: initial commit

---
+++ prettier.config.js
@@ -0,0 +1,15 @@
+/**
+ * @see https://prettier.io/docs/en/configuration.html
+ * @type {import("prettier").Config}
+ */
+
+const config = {
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  printWidth: 80,
+  trailingComma: "all",
+  bracketSpacing: true,
+};
+
+export default config;
